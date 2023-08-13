460e4c4f1d31cf51ff03514cb19d36cc3788d3a8
Waqar <waqarsanai58@gmail.com>
2023-08-12 23:43:47 -0700

    Use products.js file

--- data/products.js
+++ data/products.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function () {
 const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -658,3 +659,4 @@ const products = [
     ]
   }
 ];
+});
